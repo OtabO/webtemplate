1f28b925b53111551aad46c3a85e4c8e1dcdab0a
zhangsx <13611924659@126.com>
2017-06-08 09:14:12 +0800

    Initial commit

---
+++ src/main/java/web/template/TestController.java
@@ -0,0 +1,27 @@
+package web.template;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by zsx on 2017/6/8.
+ */
+@Controller
+public class TestController {
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public @ResponseBody String hello(){
+        return "hello";
+    }
+
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping(value = "/aa",method = RequestMethod.GET)
+    public String aa(){
+        return "aa";
+    }
+}
